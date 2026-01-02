7793b826276d711b9819a620d1a86c8040078c50
Nicholas <197684491+flitewidgeon@users.noreply.github.com>
2026-01-03 11:37:29 +1300

    Create a function 'operate' that takes an operator and two numbers and calls one of the basic math functions

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ let operandA;
 let operandB;
 let operator;
 
+function operate(operator, numA, numB){
+	let result = 0;
+
+	switch(operator){
+	case "+":
+		result = add(numA, numB);
+		break;
+	case "-":
+		result = subtract(numA, numB);
+		break;
+	case "*":
+		result = multiply(numA, numB);
+		break;
+	case "/":
+		result = divide(numA, numB);
+		break;
+	}
+
+	return result;
+}
 
 // functions for basic math operators
 function add(a, b){
